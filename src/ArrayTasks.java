90f8d8b6c97aca2272bb4239bbd896d2e81c07bb
Just Somebody <doctorcat451@gmail.com>
2022-04-26 20:15:01 +0300

    added byPriority  to ArrayTasks

--- src/ArrayTasks.java
+++ src/ArrayTasks.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -17,17 +20,63 @@ public class ArrayTasks implements Serializable {
     }
     public void reviewAll()
     {
-        for(Task allTasks:myTasksList) {
-            System.out.println(allTasks);
-        }
+        myTasksList.stream()
+                .forEach(System.out::println);
         }
         public void byPriority(){
-            for (Task person: myTasksList) {
+            boolean flag;
+            byte choice=-1;
 
-                if (person.getPriority() == "planned") {
-
-                    System.out.println(myTasksList);
+            BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Which priority of task u wish to see");
+            do {
+                flag = false;
+                try {
+                    System.out.println("1 - Planned");
+                    System.out.println("2 - Urgent");
+                    System.out.println("3 - Delegated");
+                    System.out.println("4 - Trivial");
+                    String s=br1.readLine();
+                    if (s.equals("")) {
+                        choice=0;
+                    } else choice = Byte.parseByte(s);
+                    if (choice < 0 || choice > 4) throw new MenuExceptions("Incorrect menu item");
+                } catch (NumberFormatException | IOException e) {
+                    System.out.println("Entered number is incorrect");
+                    System.out.println(e.getMessage());
+                    flag = true;
+                } catch (MenuExceptions e1) {
+                    System.out.println(e1.getMessage());
+                    flag = true;
                 }
+            }  while (flag);
+            switch (choice) {
+                case 1:
+                    myTasksList.stream()
+                            .filter(x -> x.getPriority().equals("planned"))
+                            .forEach(System.out::println);
+                    System.out.println(" ");
+                    break;
+                case 2:
+                    myTasksList.stream()
+                            .filter(x -> x.getPriority().equals("urgent"))
+                            .forEach(System.out::println);
+                    System.out.println(" ");
+                    break;
+                case 3:
+                    myTasksList.stream()
+                            .filter(x -> x.getPriority().equals("delegated"))
+                            .forEach(System.out::println);
+                    System.out.println(" ");
+                    break;
+                case 4:
+                    myTasksList.stream()
+                            .filter(x -> x.getPriority().equals("trivial"))
+                            .forEach(System.out::println);
+                    System.out.println(" ");
+                    break;
+                default:
+                    break;
             }
     }
     }
