6f1834d79afe7d6d6d20cf06f262e80d88ffe18a
Shevchenko Oleksandr <Aleksandr.A.Shev@ya.ru>
2022-04-30 15:54:10 +0300

    Override toString in Enum Classes: TaskPriority, TaskStatus

--- src/TaskPriority.java
+++ src/TaskPriority.java
@@ -1,3 +1,9 @@
 public enum TaskPriority {
     PLANNED, URGENT, DELEGATED, TRIVIAL;
+
+    @Override
+    public String toString() {
+        String priorityName = super.toString();
+        return priorityName.substring(0, 1) + priorityName.substring(1).toLowerCase();
+    }
 }
--- src/TaskStatus.java
+++ src/TaskStatus.java
@@ -1,3 +1,9 @@
 public enum TaskStatus {
     NEW, CLOSED, DONE, IN_PROGRESS;
+
+    @Override
+    public String toString() {
+        String priorityName = super.toString();
+        return priorityName.substring(0, 1) + priorityName.substring(1).toLowerCase();
+    }
 }
